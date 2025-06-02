c60a289d4c2c7bd3e9ca380bfaa3e49c9fe647d0
luversof <luversof@hanmail.net>
2025-06-02 16:14:19 +0900

    Refactor `TextEncryptorTests` to use parameterized tests with enum data.
    - Replace `@Test` with `@ParameterizedTest` using `EnumSource`.
    - Add `TextEncryptorTestsData` enum for test data management.
    - Simplify encryption test logic using `TextEncryptorFactories`.
    - Remove redundant salt and key generation code.
    - Improve test readability and maintainability.
    - Introduce structured test data for better scalability.

--- src/test/java/io/github/luversof/boot/security/crypto/TextEncryptorTests.java
+++ src/test/java/io/github/luversof/boot/security/crypto/TextEncryptorTests.java
@@ -3,9 +3,10 @@ package io.github.luversof.boot.security.crypto;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.core.io.support.SpringFactoriesLoader;
 import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.keygen.KeyGenerators;
 
 import io.github.luversof.boot.security.crypto.encrypt.BlueskyTextEncryptor;
 import io.github.luversof.boot.security.crypto.factory.TextEncryptorFactories;
@@ -14,47 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class TextEncryptorTests {
 
-	@Test
-	void encrypTest() {
-		var text = "test text!!!";
-		
-		// create random salt
-		var salt = KeyGenerators.string().generateKey();
-		log.debug("salt : {}", salt);
-		
-		log.debug("keyGenerator : {}", KeyGenerators.string().generateKey());
-		
-		{
-			var encryptor = Encryptors.text("password", "076e1bf7569c999e");
-			var encryptText = encryptor.encrypt(text);
-			log.debug("encryptText : {}", encryptText);
-			var decryptText = encryptor.decrypt(encryptText);
-			log.debug("decryptText : {}, {}", text.equals(decryptText), decryptText);
-		}
-		
-		{
-			var encryptor = Encryptors.delux("pass", "076e1bf7569c999e");
-			var encryptText = encryptor.encrypt(text);
-			log.debug("encryptText : {}", encryptText);
-			var decryptText = encryptor.decrypt(encryptText);
-			log.debug("decryptText : {}, {}", text.equals(decryptText), decryptText);
-		}
-		
-		{
-			var encryptor = Encryptors.noOpText();
-			var encryptText = encryptor.encrypt(text);
-			log.debug("encryptText : {}", encryptText);
-			var decryptText = encryptor.decrypt(encryptText);
-			log.debug("decryptText : {}, {}", text.equals(decryptText), decryptText);
-		}
-		
-		{
-			var encryptor = TextEncryptorFactories.createDelegatingTextEncryptor();
-			var encryptText = encryptor.encrypt(text);
-			log.debug("encryptText : {}", encryptText);
-			var decryptText = encryptor.decrypt(encryptText);
-			log.debug("decryptText : {}, {}", text.equals(decryptText), decryptText);
-		}
+	@ParameterizedTest
+	@EnumSource(TextEncryptorTestsData.class)
+	void encrypTest(TextEncryptorTestsData data) {
+		var text = TextEncryptorFactories.createDelegatingTextEncryptor().encrypt(data.getEncryptedText());
+		log.debug("text : {}", text);
 		
 	}
 	
